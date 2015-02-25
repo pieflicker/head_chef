a51144f9c53dfd908e45dfdc26c27f9f07d4140f
pieflicker <nesbitt.greg@gmail.com>
2015-02-25 17:47:40 -0500

    working on getting sorcery to work

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,30 @@
 HeadChef::Application.routes.draw do
+  #get "user_sessions/new"
+  #get "user_sessions/create"
+  #get "user_sessions/destroy"
+  #get "sessions/new"
+  #get "users/new"
+  root :to => 'users#index'
+  resources :user_sessions
+  resources :users
+
+  get 'login' => 'user_sessions#new', :as => :login
+  post 'logout' => 'user_sessions#destroy', :as => :logout
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+ 
+
+#These were commented out to make sorcery work
+
+#   root 'welcome#index'
+#  get "logout" => "sessions#destroy", :as => "logout"
+# get "login" => "sessions#new", :as => "login"
+#  get "signup" => "users#new", :as => "signup"
+#  resources :users
+#  resources :sessions
+  
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
